37f37326fbc7b426e156aff7bfc048d24d05d640
sed74 <fede.sed74@gmail.com>
2017-05-26 11:57:56 +0200

    Release 1.0 All activities implemented. Sorting corrected. New app Icon.

--- app/src/main/java/com/sed/federico/prontosoccorsoligura/MyApplication.java
+++ app/src/main/java/com/sed/federico/prontosoccorsoligura/MyApplication.java
@@ -26,7 +26,7 @@ public class MyApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        loadPostazione();
+//        loadPostazione();
     }
 
     @Override
@@ -36,40 +36,40 @@ public class MyApplication extends MultiDexApplication {
     }
 
 
-    private void loadPostazione() {
-        FirebaseDatabase mPostazioniDB = FirebaseDatabase.getInstance();
-        mPostazioniDB.setPersistenceEnabled(true);
-        final DatabaseReference myRef = mPostazioniDB.getReference("postazione");
-        globalPostazioni = new SparseArray<>();
-        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-//                ArrayList<Postazione> postazioni = new ArrayList<>();
-                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    // TODO: handle the post
-                    Postazione lPostazione = postSnapshot.getValue(Postazione.class);
-//                    int lID = Integer.valueOf(postSnapshot.getKey());
-//                    globalPostazioni.put(lID, lPostazione);
-//                    myRef.child(lID).child("descrizione").setValue(lPostazione.getName());
-
-                }
-
-//                myRef.setValue(postazioni);
-                Log.d("MyApplication", "Value is: " + globalPostazioni.size());
-//                Toast.makeText(MainActivity.this, "Record scaricati: " + postazioni.size(),
-//                        Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-//                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
-
-    }
+//    private void loadPostazione() {
+//        FirebaseDatabase mPostazioniDB = FirebaseDatabase.getInstance();
+//        mPostazioniDB.setPersistenceEnabled(true);
+//        final DatabaseReference myRef = mPostazioniDB.getReference("postazione");
+//        globalPostazioni = new SparseArray<>();
+//        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+////                ArrayList<Postazione> postazioni = new ArrayList<>();
+//                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+//                    // TODO: handle the post
+//                    Postazione lPostazione = postSnapshot.getValue(Postazione.class);
+////                    int lID = Integer.valueOf(postSnapshot.getKey());
+////                    globalPostazioni.put(lID, lPostazione);
+////                    myRef.child(lID).child("descrizione").setValue(lPostazione.getName());
+//
+//                }
+//
+////                myRef.setValue(postazioni);
+//                Log.d("MyApplication", "Value is: " + globalPostazioni.size());
+////                Toast.makeText(MainActivity.this, "Record scaricati: " + postazioni.size(),
+////                        Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+////                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+//
+//    }
 
     public String getPostazioneName(int id) {
         return globalPostazioni.get(id).getName();
