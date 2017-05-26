0af0bb9df27b83dea9966fc4140958e6197df407
sed74 <fede.sed74@gmail.com>
2017-05-26 11:57:56 +0200

    Release 1.0 All activities implemented. Sorting corrected.

--- app/src/main/java/com/sed/federico/prontosoccorsoligura/Mission/MissionActivity.java
+++ app/src/main/java/com/sed/federico/prontosoccorsoligura/Mission/MissionActivity.java
@@ -201,9 +201,9 @@ public class MissionActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
         this.setTitle(mCentraleName);
         //noinspection SimplifiableIfStatement
+        int id = item.getItemId();
         if (id == R.id.action_refresh) {
             refreshView(true);
 
